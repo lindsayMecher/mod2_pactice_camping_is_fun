8dfae26a37e693de088d1827b558c103baea349a
lindsayMecher <lindsaymecher@gmail.com>
2020-10-14 09:02:41 -0500

    progress

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,21 @@
 class CampersController < ApplicationController
+
+  before_action :find_camper, only: [:show]
+
   def index
     @campers = Camper.all
   end
+
+  def show
+  end 
+
+  def new
+  end 
+
+
+  private
+
+  def find_camper
+    @camper = Camper.find(params[:id])
+  end 
 end
